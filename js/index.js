96b4c211df58d2e3a78ffe83b2ca030b9703dd30
Jaman-Shah <jamanshah5400@gmail.com>
2024-02-19 00:21:40 +0600

    update ticket area functionality by clicking seats finished

--- js/index.js
+++ js/index.js
@@ -1,28 +1,57 @@
-// Step - 1: getting the total seat counter
+//getting the values with the help of id
 
 const totalSeatCounter = document.getElementById("total-seat-counter");
-
 let totalSeatCounterValue = parseFloat(totalSeatCounter.innerText);
 
-// Step - 2: getting the total ticket bought value
-
 const totalSeatBought = document.getElementById("total-seat-bought");
-
 let totalSeatBoughtValue = parseFloat(totalSeatBought.innerText);
-console.log(totalSeatBoughtValue);
 
-// Step - 3: adding click event for every button seats with the help of seats button container
+const totalSeatPrice = document.getElementById("total-seat-price");
+let totalSeatPriceValue = parseFloat(totalSeatPrice.innerText);
+
+const grandPrice = document.getElementById("grand-total-price");
+let grandPriceValue = parseFloat(grandPrice.innerText);
+
+//adding click event for every button seats with the help of seats button container
 
 const seatButtonsContainer = document.getElementById("seat-buttons-container");
 
 seatButtonsContainer.addEventListener("click", function (e) {
-  if (e.target.tagName === "BUTTON") {
-    totalSeatCounterValue -= 1; // decreasing the total seats value
+  const targetedButton = e.target;
 
-    totalSeatCounter.innerText = totalSeatCounterValue; //updating the value of total seats
+  if (targetedButton.tagName === "BUTTON") {
+    if (totalSeatBoughtValue < 4) {
+      // updating the values where needed
 
-    totalSeatBoughtValue += 1; //increasing the value of total seat bought
+      totalSeatCounterValue -= 1;
+      totalSeatCounter.innerText = totalSeatCounterValue;
 
-    totalSeatBought.innerText = totalSeatBoughtValue; // updating the value of total seat bought
+      totalSeatBoughtValue += 1;
+      totalSeatBought.innerText = totalSeatBoughtValue;
+
+      totalSeatPriceValue += 550;
+      totalSeatPrice.innerText = totalSeatPriceValue;
+      grandPrice.innerText = totalSeatPriceValue;
+
+      // Getting  the update ticket  parent element, creating model and appending it to the parent
+
+      const parentElement = document.getElementById("update-ticket-area");
+
+      const htmlCode = `
+      <div class="flex justify-between mt-4">
+        <h1>${targetedButton.innerText}</h1>
+         <h1>Economy</h1>
+        <h1>550</h1>
+      </div>
+      `;
+      parentElement.insertAdjacentHTML("beforeend", htmlCode);
+
+      // styling the selected buttons
+      targetedButton.style.backgroundColor = "green"; //setting the bg color
+      targetedButton.style.color = "white"; //setting the text color
+      targetedButton.setAttribute("disabled", true); // disabling the seat selected
+    } else {
+      alert("You cannot buy more than 4 seats");
+    }
   }
 });
